f34b6091d703c594e5b84e527bde109c4267b0e8
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-19 13:06:46 -0700

    solution complete

--- Solution.java
+++ Solution.java
@@ -1,5 +1,18 @@
+import java.util.*;
+
 public class Solution {
     public int removeDuplicates(int[] nums) {
-        return 0;
+        HashSet<Integer> numsSet = new HashSet<>();
+
+        for (int num : nums) {
+            numsSet.add(num);
+        }
+        int index = 0;
+        for (int num : numsSet) {
+            nums[index] = num;
+        }
+
+
+        return numsSet.size();
     }
 }
