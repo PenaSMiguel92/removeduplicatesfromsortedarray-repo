f4fd71f1d974ddb439cd9f56d194f9f2730e04aa
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-19 13:20:37 -0700

    first attempt
    solution does not account for hashset not retaining order of elements

--- Main.java
+++ Main.java
@@ -1,13 +1,14 @@
 public class Main { 
     public static void main(String[] args) { 
         Solution sol = new Solution();
-        int[] nums = new int[] { 1, 1, 2 };
-        int[] expectedNums = new int[] { 1, 2 };
+        int[] nums = new int[] { -3, -1, 0, 0, 0, 3, 3};
+        int[] expectedNums = new int[] { -3, -1, 0, 3};
         int k = sol.removeDuplicates(nums);
 
-        assert k == expectedNums.length;
+        System.out.println(k == expectedNums.length);
         for (int i = 0; i < k; i++) {
-            assert nums[i] == expectedNums[i];
+            System.out.println(nums[i]);
+            System.out.println(nums[i] == expectedNums[i]);
         }
     } 
 }
--- Solution.java
+++ Solution.java
@@ -1,15 +1,16 @@
-import java.util.*;
+import java.util.HashSet;
 
 public class Solution {
     public int removeDuplicates(int[] nums) {
         HashSet<Integer> numsSet = new HashSet<>();
-
         for (int num : nums) {
             numsSet.add(num);
         }
         int index = 0;
         for (int num : numsSet) {
+            System.out.println(num);
             nums[index] = num;
+            index++;
         }
 
 
